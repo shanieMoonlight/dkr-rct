971d31b1030be979b14b50d4e2cff72d3c1c4da8
shanieMoonlight <shanebrannick@yahoo.com>
2024-01-24 20:21:00 +0000

    added  travis yml file

--- src/App.test.js
+++ src/App.test.js
@@ -11,6 +11,6 @@ test('renders learn react link', () => {
 test('renders learn react link 2', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).not.toBeInTheDocument();
 });
 
